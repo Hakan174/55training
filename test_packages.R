634984ee0d3c4cc6be640784830029a043009636
Hakan174 <haki7050@gmail.com>
2024-09-20 12:42:12 +0200

    Update test_packages.R

--- test_packages.R
+++ test_packages.R
@@ -2,6 +2,7 @@ library(RPostgres)
 
 library(tidyverse) # load many packages often used in data science
 Nye version hej hej sjkallsakjdkljsadkljasdjklaskldasldjlka
+forkert kode
 sadasdasdsadasdas
 jkjlasjkldasjkldjkasld gg
 66666666666666
